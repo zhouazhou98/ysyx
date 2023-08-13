9a2442616c8bedf4732524a9a7fff1876ce1ccc6
tracer-ysyx <tracer@ysyx.org>
2023-08-13 11:25:56 +0800

    > compile NEMU ysyx_22040000 周岩瑾 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:25:56 up 53 min,  1 user,  load average: 0.46, 0.41, 0.56

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,33 +1,6 @@
-#include <stdio.h>	// printf()
-#include <stdlib.h>	// rand()
-#include <assert.h>	// assert()
-#include "verilated.h"	// VerilatedContext
-#include "Vtop.h"	// Vtop
+#include <stdio.h>
 
-
-int main(int argc, char ** argv) {
-	// heap 上分配 VerilatedContex
-	VerilatedContext * context_p = new VerilatedContext;
-	context_p->commandArgs(argc, argv);
-
-	// heap 上分配 Vtop
-	Vtop * top_p = new Vtop{context_p};
-
-	int i = 0;
-	while ( ++i < 100000 ) {
-		int a = rand() & 1;
-		int b = rand() & 1;
-
-		top_p->a = a;
-		top_p->b = b;
-		top_p->eval();
-		assert(top_p->f == (a^b) );
-		context_p->timeInc(1);
-	}
-	
-
-	delete top_p;
-	delete context_p;
-
-	return 0;
+int main() {
+  printf("Hello, ysyx!\n");
+  return 0;
 }
