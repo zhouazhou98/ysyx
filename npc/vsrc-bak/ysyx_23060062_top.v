004b6aca89d433a2b6e4ea4942729584e1f9aaec
tracer-ysyx <tracer@ysyx.org>
2023-09-28 09:38:07 +0800

    > compile NEMU-ZHOU ysyx_22040000 周岩瑾 Linux vm 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:38:07 up 4 days, 10:58,  1 user,  load average: 0.73, 0.50, 0.44

--- npc/vsrc-bak/ysyx_23060062_top.v
+++ npc/vsrc-bak/ysyx_23060062_top.v
@@ -1,10 +1,11 @@
 module ysyx_23060062_top (
+	input clk,
 	// 通用寄存器 x0 ~ x31
 	input wire [4:0] regs,
 	input wire reg_read,
 	input wire reg_write,
-	input wire [31:0] reg_read,
-	output wire [31:0] reg_write,
+	input wire [31:0] reg_write,
+	output wire [31:0] reg_read,
 
 	// pc
 	input wire [31:0] pc_write,
@@ -19,9 +20,16 @@ module ysyx_23060062_top (
 
 );
 
-// TODO: 时钟呢？什么时候需要时钟？
-// 	取指令的时候需要时钟？
-
+// TODO: 时钟呢？什么时候需要时钟？ 
+// 	1. 取指令的时候需要时钟？ 没有时钟 inst 寄存器会随着与 内存 的数据总线
+// 	的变化而改变，不能这样
+// 		取指令前需要将 内存读 信号，内存地址 信号提前给到 控制总线
+// 		和 地址总线，这样等 时钟 到来时可以直接从内存中读出数据
+//	取指令 -- 读取内存 --> MM 与 Reg 交换
+//
+//	2. 写入 MM：MM 需要知道处理器将 写 控制信号 和 PC 同时给到 并且在时钟
+//	到来时 将数据写入，那如果数据还没有给到呢？可能会向 MM 中写入错误的值
+//	因此需要：给数据， 给 PC， 给信号， 等待时钟信号
 
 // 0. 定义寄存器
 // TODO:
@@ -31,38 +39,13 @@ reg [31:0] pc;
 
 // TODO: 这里还是用子文件来实现通用寄存器比较好，给个写入读出接口即可
 // 	其实这里可以直接给 rd，不用在指令判断 type 时译码了，还简单省事
-reg [31:0] x0;
-reg [31:0] x1;
-reg [31:0] x2;
-reg [31:0] x3;
-reg [31:0] x4;
-reg [31:0] x5;
-reg [31:0] x6;
-reg [31:0] x7;
-reg [31:0] x8;
-reg [31:0] x9;
-reg [31:0] x10;
-reg [31:0] x11;
-reg [31:0] x12;
-reg [31:0] x13;
-reg [31:0] x14;
-reg [31:0] x15;
-reg [31:0] x16;
-reg [31:0] x17;
-reg [31:0] x18;
-reg [31:0] x19;
-reg [31:0] x20;
-reg [31:0] x21;
-reg [31:0] x22;
-reg [31:0] x23;
-reg [31:0] x24;
-reg [31:0] x25;
-reg [31:0] x26;
-reg [31:0] x27;
-reg [31:0] x28;
-reg [31:0] x29;
-reg [31:0] x30;
-reg [31:0] x31;
+ysyx_23060062_regs regs (
+	input wire [4:0] regs,
+	input wire reg_read,
+	input wire reg_write,
+	input wire [31:0] reg_write,
+	output wire [31:0] reg_read
+);
 
 // 用来存放当前正在译码的指令
 reg [31:0] inst;
@@ -72,7 +55,7 @@ ysyx_23060062_inst_fetch ifetch (
 	.pc_in (pc),			// 用来指示从 mem 中哪个地址读取数据
 	.mem_read(mem_read),		// 用来更改 读内存 信号
 					// 控制信号：用来输出给内存（TODO:这里其实是告诉 C 语言，不过逻辑正确与否先不关系）
-	.inst_fetch (inst_fetch),	// 用来输入内存地址 R[pc] 中存储的指令
+	.inst_fetch (inst_fetch)	// 用来输入内存地址 R[pc] 中存储的指令
 );
 
 // TODO: 这里到底用 = 还是 <=，如果将 inst 改为 wire 类型显然是不合适的，内存
@@ -86,17 +69,43 @@ end
 // 		opcode [4:2] : 32 bit ?
 // 		opcode [6:5] : 对于 32 bit 指令确定 type (I, R, U, S, B, J)
 // 	TODO: 这里对 opcode[6:2] 译码可以使用 桶形移位器 
+ysyx_23060062_inst_decoder idecode (
+	.inst_16bit (inst[1:0]),
+	.inst_32bit (inst[4:2]),
+	.inst_base_opcode (inst[6:5]),
+	.opcode_en (opcode_en[31:0]),
+	.opcode_n_bit (opcode_n_bit_en[1:0])
+);
 
 
 
-
-// 3.1 	funct3 ? --> funct7 ? 
+// 3.1	type ? --> type decode
+ysyx_23060062_inst_type_decode type_decode (
+	.opcode_en (opcode_en[31:0]),
+	.opcode_n_bit (opcode_n_bit_en[2:0]),
+	.inst (inst[31:0]),
+	.src1 (src1[31:0]),
+	.src2 (src2[31:0]),
+	.imm (imm[31:0]),
+	.pc_read (pc[31:0])
+);
+// 3.2 	funct3 ? --> funct7 ? 
 // 	TODO: type 的译码需要再做个译码部件来提高效率吗？如果使用 32 位译码器
 // 	对 opcode 译码后再进行 type 选择，会不会效率不高？还是说译码部件译码同
 // 	时，所有的 type 都在对指令进行类型译码操作，最后根据译码器 32 位输出在
 // 	 6 种类型中选择？
-// 3.2	type ? --> type decode
-// //////////3.3	exec ? --> 直接到具体指令 ----------- 和 4 重复了吧？
+// 可以再接个 100 根左右的线，作为指令的使能端，输出
+ysyx_23060062_inst_inst_exec_decode inst_exec_decode (
+	.inst (inst[31:0]),
+	.memory_op (memory_op),
+	.opcode_n_bit (opcode_n_bit_en[1:0]),
+	.opcode_en (opcode_en[31:0]),
+	.src1 (src1[31:0]),
+	.src2 (src2[31:0]),
+	.imm (imm[31:0]),
+	.pc_read (pc_read)
+	.rd_output (rd_data[31:0])
+);
 
 
 
@@ -106,6 +115,7 @@ end
 
 
 // 5. 写回 TODO: 如果指令写回到 pc，能不能保证正确
+// 	写回操作需要分组讨论吗？是写到 pc 还是写道 x0~x31
 
 
 
